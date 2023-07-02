e8dc1400d518cce833d37a7b76f9fae21b548030
Zach Adler <zach8302@berkeley.edu>
2023-07-02 11:43:56 -0700

    added verification endpoint

---
+++ api/stripe_views.py
@@ -0,0 +1,30 @@
+
+from .models import User
+import stripe
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import UserSerializer
+
+class VerificationSessionView(APIView):
+
+    def post(self, request, format=None):
+        # Set your secret key. Remember to switch to your live secret key in production.
+        # See your keys here: https://dashboard.stripe.com/apikeys
+        stripe.api_key = 'sk_test_51NOtuQENXUDWXy01vZ0UjvktC1kwlhQypbLsim47lCgOsYzuQgXi2MrrQYNkGNj7xi7CeJmm7A1jMn7jeVeyUJv400MtAbnhLN'
+
+        # In the route handler for /create-verification-session:
+        # Authenticate your user.
+
+        # Create the session.
+        verification_session = stripe.identity.VerificationSession.create(
+            type='document',
+            metadata={
+                'user_id': '{{USER_ID}}',
+            },
+        )
+
+        # Return only the client secret to the frontend
+        client_secret = verification_session.client_secret
+        return Response({'client_secret' : client_secret}, status=status.HTTP_200_OK)
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import AuthenticateView, VerifyView, IsAuthenticatedView
 from .auth_views import MeView
+from .stripe_views import VerificationSessionView
 
 urlpatterns = [
     path('authenticate', AuthenticateView.as_view()),
     path('verify', VerifyView.as_view()),
     path('is_authenticated', IsAuthenticatedView.as_view()),
-    path('me', MeView.as_view())
+    path('me', MeView.as_view()),
+    path('create-verification-session', VerificationSessionView.as_view())
     ]
