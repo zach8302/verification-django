c6dde2f26c501c754b6b1b0f13a72d3b06d0980e
Zach Adler <zach8302@berkeley.edu>
2023-07-02 12:50:33 -0700

    potential stripe verification implemented

--- api/stripe_views.py
+++ api/stripe_views.py
@@ -7,26 +7,72 @@ from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import UserSerializer
 
-class VerificationSessionView(APIView):
+stripe.api_key = 'sk_test_51NOtuQENXUDWXy01vZ0UjvktC1kwlhQypbLsim47lCgOsYzuQgXi2MrrQYNkGNj7xi7CeJmm7A1jMn7jeVeyUJv400MtAbnhLN'
 
+class VerificationSessionView(APIView):
+    permission_classes = [IsAuthenticated]
     def post(self, request, format=None):
         # Set your secret key. Remember to switch to your live secret key in production.
         # See your keys here: https://dashboard.stripe.com/apikeys
-        stripe.api_key = 'sk_test_51NOtuQENXUDWXy01vZ0UjvktC1kwlhQypbLsim47lCgOsYzuQgXi2MrrQYNkGNj7xi7CeJmm7A1jMn7jeVeyUJv400MtAbnhLN'
-
         # In the route handler for /create-verification-session:
         # Authenticate your user.
 
         # Create the session.
+        email = request.user.email
+
         verification_session = stripe.identity.VerificationSession.create(
             type='document',
             metadata={
-                'user_id': '{{USER_ID}}',
+                'user_id': email,
             },
         )
 
         # Return only the client secret to the frontend
         url = verification_session.url
+        print(verification_session.metadata.user_id)
 
 
         return Response({'url' : url}, status=status.HTTP_200_OK)
+    
+class VerificationWebhookView(APIView):
+
+    def post(self, request, format=None):
+        signature = request.headers.get('stripe-signature')
+        payload = request.data
+        endpoint_secret = "we_1NPVtfENXUDWXy01DGHsenD4"
+
+        # Verify webhook signature and extract the event.
+        # See https://stripe.com/docs/webhooks/signatures for more information.
+        try:
+            event = stripe.Webhook.construct_event(
+            payload=payload,
+            sig_header=signature,
+            secret=endpoint_secret,
+            )
+        except ValueError as e:
+            # Invalid payload.
+            return Response(status=400)
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid Signature.
+            return Response(status=400)
+
+        print(event)
+        if event['type'] == 'identity.verification_session.verified':
+            print("All the verification checks passed")
+            verification_session = event.data.object
+            email = verification_session.metadata.user_id
+            user = User.objects.get(email=email)
+            user.verified = True
+            user.save()
+
+        elif event['type'] == 'identity.verification_session.requires_input':
+            print("At least one verification check failed")
+            verification_session = event.data.object
+            if verification_session.last_error.code == 'document_unverified_other':
+                print("The document was invalid")
+            elif verification_session.last_error.code == 'document_expired':
+                print("The document was expired")
+            elif verification_session.last_error.code == 'document_type_not_supported':
+                print("The document type was not supported")
+            else:
+                print("other error code")
--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import AuthenticateView, VerifyView, IsAuthenticatedView
 from .auth_views import MeView
-from .stripe_views import VerificationSessionView
+from .stripe_views import VerificationSessionView, VerificationWebhookView
 
 urlpatterns = [
     path('authenticate', AuthenticateView.as_view()),
     path('verify', VerifyView.as_view()),
     path('is-authenticated', IsAuthenticatedView.as_view()),
     path('me', MeView.as_view()),
-    path('create-verification-session', VerificationSessionView.as_view())
+    path('create-verification-session', VerificationSessionView.as_view()),
+    path('webhook', VerificationWebhookView.as_view())
     ]
